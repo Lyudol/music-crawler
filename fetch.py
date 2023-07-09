b81f7ecf5b74126ed93de4105990fadcb42fccc0
Tom342 <tomikhoh@gmail.com>
2023-07-09 18:09:57 +0100

    Multithreaded finding
    Finding songs to add lyrics to is now multithreaded, using 5 chrome windows.
    
    Able to be ran headless.
    
    Found songs are added to a text file.
    
    Stability/Error handling to do.

--- fetch.py
+++ fetch.py
@@ -15,23 +15,59 @@ sw_options = {
     'disable_encoding': True,  # Ask the server not to compress the response
     'disable_capture': True
 }
+
+
+# options.add_argument(r"--user-data-dir=C:\Users\PC\AppData\Local\Google\Chrome\User Data") #e.g. C:\Users\You\AppData\Local\Google\Chrome\User Data
+options.add_argument("--disable-infobars")
+# options.add_argument("--start-maximized")
+options.add_argument("--disable-extensions")
+options.add_argument('--window-size=1920,1080')
+options.add_argument("--headless")
+options.add_experimental_option("detach", True) 
+
+
+options.add_argument(r'--profile-directory=Profile 1')
 driver = webdriver.Chrome(executable_path=r'F:\Downloads\chromedriver_win32 (2)\chromedriver.exe', chrome_options=options)
-# driver.maximize_window()
-options.add_argument(r"--user-data-dir=C:\Users\PC\AppData\Local\Google\Chrome\User Data") #e.g. C:\Users\You\AppData\Local\Google\Chrome\User Data
-options.add_argument(r'--profile-directory=Profile 2') #e.g. Profile 3
-options.add_experimental_option("detach", True)
+driver.minimize_window()
+
+options.add_argument('--window-size=688,1440')
+
+options.add_argument(r'--profile-directory=Profile 2') 
+driverfinder1 = webdriver.Chrome(executable_path=r'F:\Downloads\chromedriver_win32 (2)\chromedriver.exe', chrome_options=options)
+driverfinder1.set_window_position(-862, -1440)
+
+
+options.add_argument(r'--profile-directory=Profile 3') 
+driverfinder2 = webdriver.Chrome(executable_path=r'F:\Downloads\chromedriver_win32 (2)\chromedriver.exe', chrome_options=options)
+driverfinder2.set_window_position(-174, -1440)
+
+options.add_argument(r'--profile-directory=Profile 4') 
+driverfinder3 = webdriver.Chrome(executable_path=r'F:\Downloads\chromedriver_win32 (2)\chromedriver.exe', chrome_options=options)
+driverfinder3.set_window_position(514, -1440)
+
+options.add_argument(r'--profile-directory=Profile 5')
+driverfinder4 = webdriver.Chrome(executable_path=r'F:\Downloads\chromedriver_win32 (2)\chromedriver.exe', chrome_options=options)
+driverfinder4.set_window_position(1202, -1440)
+
+options.add_argument(r'--profile-directory=Profile 6')
+driverfinder5 = webdriver.Chrome(executable_path=r'F:\Downloads\chromedriver_win32 (2)\chromedriver.exe', chrome_options=options)
+driverfinder5.set_window_position(1890, -1440)
 
 
 
 def openwebsite(website):
     driver.get(website)
+def pagesource(drivertouse):
+    return drivertouse.page_source
+def openfinderwebsite(website,drivertouse):
+    drivertouse.get(website)
 
-def clickcookie():
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[9]/div[2]/div/div[1]/div/div[2]/div/button[2]"))).click()
-def load():
-    element = driver.find_element(By.XPATH, "/html/body/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]")
-    driver.execute_script("arguments[0].scrollIntoView(true);", element);
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]"))).click()
+def clickcookie(drivertouse):
+    WebDriverWait(drivertouse, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[9]/div[2]/div/div[1]/div/div[2]/div/button[2]"))).click()
+def load(drivertouse):
+    element = drivertouse.find_element(By.XPATH, "/html/body/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]")
+    drivertouse.execute_script("arguments[0].scrollIntoView(true);", element);
+    WebDriverWait(drivertouse, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]"))).click()
 
 def geniusfetch():
     openwebsite("https://genius.com/Jay-z-holy-grail-lyrics")
--- finder.py
+++ finder.py
@@ -2,24 +2,28 @@ import fetch
 import html_to_json
 import json
 import time
+import math
+from threading import Thread
 #tracks = jsonified["html"][0]["body"][0]["div"][1]["div"][0]["div"][1]["div"][0]["div"][0]["div"][0]["div"][1]["div"][0]["div"][0]["div"][1]["div"][0]["div"][0]["ul"][0]["li"] for all tracks
 #tracks[8]["div"][0]["div"][0]["img"][0]["_attributes"]["alt"] for each name
 
+drivers = [fetch.driverfinder1,fetch.driverfinder2,fetch.driverfinder3,fetch.driverfinder4,fetch.driverfinder5]
 
-def opensite(website):
-    fetch.openwebsite(website)
 
-def getjson():
-    html = fetch.driver.page_source
+def opensite(website,drivertouse):
+    fetch.openfinderwebsite(website,drivertouse)
+
+def getjson(drivertouse):
+    html = fetch.pagesource(drivertouse)
     jsonified = html_to_json.convert(html)
 
     towrite = jsonified
-    f = open("json.txt", "w")
+    f = open("json"+str(drivers.index(drivertouse))+".txt", "w")
     f.write(json.dumps(towrite))
     f.close()
 
-def gettracks():
-    with open("json.txt", "r") as file:
+def gettracks(drivertouse):
+    with open("json"+str(drivers.index(drivertouse))+".txt", "r") as file:
                 jsonified = file.read()
                 jsonified = json.loads(jsonified)
 
@@ -46,21 +50,49 @@ def findtoadd(tracks):
                     tracknumber +=1
     return found
 
-def main():
+def getter(drivertouse):
     for i in range(3):
-        fetch.load()        # loads thrice
-        time.sleep(2)
-    getjson()               # gets json of page
-    tracks = gettracks()    # gets all tracks
+        fetch.load(drivertouse)        # loads thrice
+        
+    getjson(drivertouse)               # gets json of page
+    tracks = gettracks(drivertouse)    # gets all tracks
     found = findtoadd(tracks) # finds ones that needs lyrics
     return found #returns
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']  
-for letter in alphabet:   # searches each letter
-    opensite("https://www.musixmatch.com/search/" + letter + "/tracks")
-    found = main()
-    file = open('songsfound.txt','a', encoding="utf-8")             # adds lyric to add to file
-    for song in found:
-        file.write(song+"\n")
-    file.close()
+
+def main(drivertouse,terms):
+    opensite("https://www.musixmatch.com/search/tracks",drivertouse)
+    fetch.clickcookie(drivertouse)
+    for term in terms:   # searches each letter
+        opensite("https://www.musixmatch.com/search/" + term + "/tracks", drivertouse)
+        found = getter(drivertouse)
+        file = open('songsfound.txt','a', encoding="utf-8")             # adds lyric to add to file
+        for song in found:
+            file.write(song+"\n")
+        file.close()
           
+
+#n = amount of drivers
+
+
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+wordfile = open("words.txt", "r")
+data = wordfile.read()
+lst = data.split("\n")
+
+
+
+chunkedlist = list(chunks(lst,int(math.ceil(len(lst)/len(drivers)))))
+
+
+
+
+Thread(target=main, args=(drivers[0],chunkedlist[0],)).start()
+Thread(target=main, args=(drivers[1],chunkedlist[1],)).start()
+Thread(target=main, args=(drivers[2],chunkedlist[2],)).start()
+Thread(target=main, args=(drivers[3],chunkedlist[3],)).start()
+Thread(target=main, args=(drivers[4],chunkedlist[4],)).start()
